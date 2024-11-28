1aa14e6c7b0cd59a8a3660d9ca384710053d0e08
ncquan04 <chiquan02122004@gmail.com>
2024-11-28 23:01:54 +0700

    SongDetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,28 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Loader, Error, RelatedSongs } from "../components";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+
+const SongDetails = () => {
+    const dispatch = useDispatch();
+    const { songid } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data: songData, isFetching: isFetchingSongDetails} = useGetSongDetailsQuery({ songid });
+
+    return (
+        <div className="flex flex-col">
+            {/* <DetailsHeader artistId={artistId} songData={songData}/> */}
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">Lyrics: </h2>
+                <div className="mt-5">
+                    {songData?.resources?.songs?.attributes?.hasLyrics ? songData?.lyrics.attributes.text.map((line, i) => (
+                        <p className="text-gray-400 text-base my-1">{line}</p>
+                    )) : <p className="text-gray-400 text-base my-1">No lyrics found</p>} 
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default SongDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('x-rapidapi-key', '54c8faa2fbmsh08a94c147fd669ep18519djsn17d5685dbbe3');
 
@@ -12,11 +12,15 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({
-            query: () => `/charts/world?country_code=DZ`,
+            query: () => `/v1/charts/world?country_code=DZ`,
+        }),
+        getSongDetails: builder.query({
+            query: ({songid}) => `/v2/tracks/details?track_id=${songid}`,
         }),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongDetailsQuery,
 } = shazamCoreApi;
